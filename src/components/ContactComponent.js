5c83944614ca840ee1e29ee527dcce27e88822ae
ayoubben5 <60117038+ayoubben5@users.noreply.github.com>
2020-06-24 18:07:58 +0200

    Update ContactComponent.js

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
 import {BreadcrumbItem,Breadcrumb,Button,Label,Col,Row} from 'reactstrap';
-import {Control, LocalForm, Errors} from 'react-redux-form';
+import {Control, Form, Errors, actions} from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -24,7 +24,7 @@ class Contact extends Component {
     handleSubmit(values){
         console.log("current State is "+JSON.stringify(values));
         alert("current State is "+JSON.stringify(values));
-        
+        this.props.resetFeedbackForm();
         
 }
 
@@ -74,7 +74,7 @@ class Contact extends Component {
                     <h3>Send us Your Feedback</h3>
                 </div>
                 <div className="col-12 col-md-9">
-                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                     <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -195,7 +195,7 @@ class Contact extends Component {
                                 </Button>
                             </Col>
                         </Row>
-                    </LocalForm>
+                    </Form>
                 </div>
             </div>
         </div>
